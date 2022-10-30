1d9573f032a3e357c28aec93f8171d4f8a758960
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2022-10-30 16:30:51 -0300

    [add] db connection

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const  cors = require("cors");
+const { dbConnection } = require("./src/db");
 require('dotenv').config()
 
 const app = express();
 app.use(cors())
-
+dbConnection()
 app.listen(5000, () => {
     console.log("server running on port 5000");
   });
