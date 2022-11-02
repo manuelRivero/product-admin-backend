d189247a4edc646cb03d463c6c6e499fcea0074a
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2022-11-02 20:19:48 -0300

    [add] user model

---
+++ src/models/user/index.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  avatar: {
+    type: String,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 8,
+    validate(value) {
+      if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+        throw new Error(
+          "Password must contain at least one letter and one number"
+        );
+      }
+    },
+  },
+  provider: {
+    type: String,
+    default: "web",
+  },
+
+}, {
+    collection: "Users"
+});
+
+module.exports = model("User", UserSchema)
