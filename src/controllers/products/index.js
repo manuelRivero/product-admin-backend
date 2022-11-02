0681c9e2a5a9ca52e2af13223b97c8aa416e3531
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2022-11-02 20:18:26 -0300

    [add] get product price filter

--- src/controllers/products/index.js
+++ src/controllers/products/index.js
@@ -19,11 +19,14 @@ const getProducts = async (req, res)=>{
   const page = Number(req.query.page) || 0;
   const regex =  new RegExp(req.query.search, 'i')
   const search = req.query.search ? {name:regex} : {};
-  const tags = req.query.tags ? {'tags.name': { $in: JSON.parse(req.query.tags) }} : {}
-
+  const minPrice = req.query.minPrice ? {$lte : Number(req.query.minPrice)} : {};
+  const maxPrice = req.query.maxPrice ? {$lte : Number(req.query.maxPrice)}: {};
+  const priceQuery = {price:{...minPrice, ...maxPrice}}
+  const tags = req.query.tags ? {'tags.name': { $in: JSON.parse(req.query.tags) }} : {};
+  
   const [products, total] = await Promise.all([
-    Product.find({...search , ...tags }).skip(page * 10).limit(10),
-    Product.count()
+    Product.find({...search , ...tags, ...priceQuery }).skip(page * 10).limit(10),
+    Product.find({...search , ...tags, ...priceQuery }).count()
   ])
   res.json({
     ok: true,
