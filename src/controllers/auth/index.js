159520febb1554713c763d31b20778bd527dcc90
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2022-11-05 22:50:22 -0300

    [add] admin login

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -24,9 +24,46 @@ const login = async (req, res) => {
   res.status(200).json({
     ok: true,
     token,
+    role: targetUser.role
   });
+};
+
+const adminLogin = async (req, res) => {
+  const { email, password } = req.body;
+
+  const targetUser = await User.findOne({email});
+  if (!targetUser) {
+    return res.status(404).json({
+      ok: false,
+      message: "Credenciales invalidas",
+    });
+  }
+
+  if(targetUser.role === "admin"){
+    
+    if (!bcript.compareSync(password, targetUser.password )) {
+      return res.status(404).json({
+        ok: false,
+        message: "Credenciales invalidas",
+      });
+    }
+  
+    const token = await generatejWT(targetUser.id, targetUser.role);
+    res.status(200).json({
+      ok: true,
+      token,
+      role: targetUser.role
+    });
+
+  }else{
+    res.status(400).json({
+      ok:false,
+      message:"Credenciales invalidas"
+    })
+  }
 };
 
 module.exports = {
   login,
+  adminLogin
 };
--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,9 +1,10 @@
 const {Router} = require("express");
-const { login } = require("../../controllers/auth");
+const { login, adminLogin } = require("../../controllers/auth");
 
 const router = Router();
 
 
 router.post('/', login)
+router.post('/admin', adminLogin)
 
 module.exports = router
