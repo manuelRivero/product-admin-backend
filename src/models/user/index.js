9777482556b3c59243301983f366f43bc1ba8326
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2022-11-11 21:33:38 -0300

    [add] likedProduct to user model

--- src/models/user/index.js
+++ src/models/user/index.js
@@ -1,51 +1,56 @@
 const { string } = require("joi");
 const { Schema, model } = require("mongoose");
 
-const UserSchema = Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-  },
-  email:{
-    type:String,
-    unique:true
-  },
-  avatar: {
-    type: String,
-  },
-  role: {
-    type: String,
-    default: "user",
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 8,
-    validate(value) {
-      if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
-        throw new Error(
-          "Password must contain at least one letter and one number"
-        );
-      }
+const UserSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
     },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    avatar: {
+      type: String,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 8,
+      validate(value) {
+        if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+          throw new Error(
+            "Password must contain at least one letter and one number"
+          );
+        }
+      },
+    },
+    provider: {
+      type: String,
+      default: "web",
+    },
+    likedProducts: [
+      { required: true, type: Schema.Types.ObjectId, ref: "Product" },
+    ],
   },
-  provider: {
-    type: String,
-    default: "web",
-  },
-
-}, {
+  {
     collection: "users",
-    timestamps: true 
-});
+    timestamps: true,
+  }
+);
 
-module.exports = model("User", UserSchema)
+module.exports = model("User", UserSchema);
