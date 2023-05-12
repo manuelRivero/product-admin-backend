e56763926347bd7d81137cd40aa355d67e4e30d4
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2023-05-12 15:05:25 -0300

    [add] notification count

--- src/notifications/controller.js
+++ src/notifications/controller.js
@@ -4,13 +4,14 @@ export const list = {
   do: async (req, res, next) => {
     const page = Number(req.query.page) || 0;
 
-    const notifications = await Notification.find()
+    const [notifications, total ] = await Promise.all([Notification.find()
       .skip(page * 10)
       .limit(10)
-      .lean();
+      .lean(), Notification.find().count()]) 
     res.status(200).json({
       ok: true,
       notifications,
+      total
     });
   },
 };
