d879297bf1b7e0bbe303aa3865e47ff412458fcd
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2024-12-29 20:37:36 -0300

    [fix] success sale email imports

--- src/emailHanlers/successSale/index.js
+++ src/emailHanlers/successSale/index.js
@@ -1,30 +1,37 @@
-import nodemailer from "nodemailer";
-import fs from "fs";
-import { promisify } from "util";
-import path from "path";
-import { fileURLToPath } from "url";
-import handlebars from "handlebars";
+const nodemailer = require("nodemailer");
+const fs = require("fs");
+const promisify = require("promisify");
+const path = require("path");
+const url = require("url");
+const handlebars = require("handlebars");
 
 const readFileAsync = promisify(fs.readFile);
 
 // Convertir import.meta.url a __dirname
-const __filename = fileURLToPath(import.meta.url);
+const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export const sendSucessEmail = async ({products, names, user, tenant, total, payment_id}) => {
+export const sendSucessEmail = async ({
+  products,
+  names,
+  user,
+  tenant,
+  total,
+  payment_id,
+}) => {
   const templateFile = await readFileAsync(
     path.resolve(__dirname, "../../templates/success-email.html"),
     "utf-8"
   );
   const template = handlebars.compile(templateFile);
-  const replacements = {products, names, user, tenant, total, payment_id};
+  const replacements = { products, names, user, tenant, total, payment_id };
   const finalHtml = template(replacements);
 
   const mailOptions = {
     from: "contacto@lorem-insights.com",
     to: "contacto@lorem-insights.com",
     subject: "¡Nuevo mensaje de un usuario!",
-    html:finalHtml,
+    html: finalHtml,
   };
 
   const transporter = nodemailer.createTransport({
@@ -32,8 +39,8 @@ export const sendSucessEmail = async ({products, names, user, tenant, total, pay
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
-      user: 'contacto@lorem-insights.com',
-      pass: '/5sM0Hat',
+      user: "contacto@lorem-insights.com",
+      pass: "/5sM0Hat",
     },
   });
 
@@ -41,8 +48,6 @@ export const sendSucessEmail = async ({products, names, user, tenant, total, pay
     await transporter.sendMail(mailOptions);
     console.log(`Correo enviado`);
   } catch (error) {
-    console.error('Error al enviar a error', error);
+    console.error("Error al enviar a error", error);
   }
-  
 };
-
