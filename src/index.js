0d786f050bd7a1eb92fa6d49f47c6e5ac476a1a2
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2025-01-02 20:59:29 -0300

    [add] sub domain -admin allowedOrigins

--- src/index.js
+++ src/index.js
@@ -31,6 +31,11 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+  next();
+});
+
 
 // app.use(session({
 //   secret: process.env.SECRETORPRIVATEKEY,
